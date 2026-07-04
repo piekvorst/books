8b18a34c31395f8c556841fba20f83030bde4de0
Steve Weston <piekvorst@outlook.com>
2026-07-04 15:22:33 +0500

    validate input

--- books.go
+++ books.go
@@ -1,7 +1,5 @@
 // problems, prioritized:
 //
-// . correctness: validate input
-//
 // . correctness: eliminate writing status after Write
 //
 // . concurrency safety: respect request's context
@@ -32,6 +30,11 @@ import (
 	"sync"
 )
 
+const (
+	maxLenTitle  = 255
+	maxLenAuthor = 255
+)
+
 type Book struct {
 	ID     int    `json:"id"`
 	Title  string `json:"title"`
@@ -42,6 +45,67 @@ var storage []*Book
 
 var mu sync.RWMutex
 
+// UserInputValid validates b, assuming that the whole data is
+// filled by the user.
+//
+// If safeError is non-nil, an error message is written there,
+// guaranteeing the content to be safe to show the client.
+func (b *Book) UserInputValid(safeError *string) bool {
+	if b.Title == "" {
+		sprintfInto(safeError, "title is empty")
+		return false
+	}
+	if len(b.Title) > maxLenTitle {
+		sprintfInto(safeError, "title is too long")
+		return false
+	}
+
+	if b.Author == "" {
+		sprintfInto(safeError, "author is empty")
+		return false
+	}
+	if len(b.Author) > maxLenAuthor {
+		sprintfInto(safeError, "author is too long")
+		return false
+	}
+
+	return true
+}
+
+// UserInputValidForCreate validates b, applying additional rules
+// assuming the input is used to create a new record.
+func (b *Book) UserInputValidForCreate(safeError *string) bool {
+	if !b.UserInputValid(safeError) {
+		return false
+	}
+
+	if b.ID != 0 {
+		sprintfInto(safeError, "ID must not be set")
+		return false
+	}
+
+	return true
+}
+
+// UserInputValidForUpdate validates b, applying additional rules
+// assuming the input is used to update an existing record.
+func (b *Book) UserInputValidForUpdate(safeError *string) bool {
+	if !b.UserInputValid(safeError) {
+		return false
+	}
+
+	if b.ID < 0 {
+		sprintfInto(safeError, "negative ID is not allowed: %v", b.ID)
+		return false
+	}
+	if b.ID == 0 {
+		sprintfInto(safeError, "ID must be set")
+		return false
+	}
+
+	return true
+}
+
 func create(w http.ResponseWriter, r *http.Request) {
 	var b Book
 	if err := json.NewDecoder(r.Body).Decode(&b); err != nil {
@@ -50,9 +114,10 @@ func create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if b.ID != 0 {
+	var validationError string
+	if !b.UserInputValidForCreate(&validationError) {
 		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "ID cannot be set\n")
+		fmt.Fprintf(w, "input is invalid: %v\n", validationError)
 		return
 	}
 
@@ -104,6 +169,13 @@ func update(w http.ResponseWriter, r *http.Request) {
 	if err := json.NewDecoder(r.Body).Decode(&b); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "cannot parse book\n")
+		return
+	}
+
+	var validationError string
+	if !b.UserInputValidForUpdate(&validationError) {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "input is invalid: %v\n", validationError)
 		return
 	}
 
@@ -161,6 +233,12 @@ func delete(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "not found\n")
+	}
+}
+
+func sprintfInto(dst *string, format string, a ...any) {
+	if dst != nil {
+		*dst = fmt.Sprintf(format, a...)
 	}
 }
 
